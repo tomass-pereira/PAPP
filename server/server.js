c933b8643d13ecfdddd89dcf8d3805a357edfd4c
tomass-pereira <tj0816499@gmail.com>
2024-12-03 12:08:09 +0000

    estudar

--- server/server.js
+++ server/server.js
@@ -7,22 +7,8 @@ const app = express();
 connectDB();
 
 app.use(express.json()); 
-app.delete('/api/movies/title/:title', async (req, res) => {
-  try {
-    // Usa o parâmetro 'nome' na URL para buscar e deletar o fisioterapeuta
-    const movies = await movies.findOneAndDelete({ title: req.params.title});
-
-    // Verifica se o fisioterapeuta foi encontrado
-    if (!movies) {
-      return res.status(404).json({ message: 'Filme não encontrado' });
-    }
-
-    // Retorna uma resposta de sucesso
-    res.status(200).json({ message: 'Filme deletado com sucesso' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Erro no servidor' });
-  }
+app.get('/api/test', (req, res) => {
+  res.send('MongoDB is connected');
 });
 
 
