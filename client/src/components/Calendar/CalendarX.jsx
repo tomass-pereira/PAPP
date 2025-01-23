c823d1bf2cd7213a06bb5fbf5993aa81bc493290
tomass-pereira <tj0816499@gmail.com>
2025-01-23 12:17:54 +0000

    calendarrrrrr

--- client/src/components/Calendar/CalendarX.jsx
+++ client/src/components/Calendar/CalendarX.jsx
@@ -4,96 +4,89 @@ import {ScheduleXCalendar, useCalendarApp} from "@schedule-x/react";
 import {createEventsServicePlugin} from "@schedule-x/events-service";
 import {createViewDay, createViewMonthGrid, createViewWeek} from "@schedule-x/calendar";
 
+ 
 
 import '@schedule-x/theme-default/dist/index.css'
 import '@sx-premium/interactive-event-modal/index.css'
-import {createInputField, createInteractiveEventModal} from "@sx-premium/interactive-event-modal";
-
+import { createEventModalPlugin } from '@schedule-x/event-modal'
 function CalendarApp() {
   const eventsService = useState(() => createEventsServicePlugin())[0];
+  const eventModal = useState(() => createEventModalPlugin())[0]
 
-  const regionInputField = createInputField({
-    label: 'Region',
-    type: 'select',
-    items: [],
-  })
-
-  const countryInputField = createInputField({
-    label: 'Country',
-    type: 'select',
-    items: [
-      { label: 'USA', value: 'USA' },
-      { label: 'Germany', value: 'Germany' },
-    ],
-    onChange: (value) => {
-      console.log(modalPlugin.formValues.value)
-      modalPlugin.formValues.value = {
-        ...modalPlugin.formValues.value,
-        region: ''
-      }
-      regionInputField.value.rerender()
-
-      if (value === 'USA') {
-        regionInputField.value = {
-          ...regionInputField.value,
-          items: [
-            { label: 'California', value: 'ca' },
-            { label: 'New York', value: 'ny' },
-          ]
-        }
-      } else if (value === 'Germany') {
-        regionInputField.value = {
-          ...regionInputField.value,
-          items: [
-            { label: 'Berlin', value: 'berlin' },
-            { label: 'Munich', value: 'munich' },
-          ]
-        }
-      }
-    }
-  });
-  const modalPlugin = useState(() => createInteractiveEventModal({
-    eventsService,
-
-    onAddEvent: (event) => {
-      console.log(event)
-      // save the event on your server
-    },
-
+  
+   
+  
     // (Optional): callback for when an event is updated
-    onDeleteEvent: (eventId) => {
-      console.log(eventId)
-      // delete the event from your server
-    },
-
-    fields: {
-      title: {},
-      startDate: {},
-      startTime: {},
-      endDate: {},
-      endTime: {},
-    },
-
-    customFields: {
-      country: countryInputField,
-      region: regionInputField,
-    }
-  }))[0];
 
   const calendar = useCalendarApp({
     views: [createViewDay(), createViewWeek(), createViewMonthGrid()],
+    calendars: {
+      personal: {
+        colorName: 'personal',
+        lightColors: {
+          main: '#f9d71c',
+          container: '#fff5aa',
+          onContainer: '#594800',
+        },
+        darkColors: {
+          main: '#fff5c0',
+          onContainer: '#fff5de',
+          container: '#a29742',
+        },
+      },
+      work: {
+        colorName: 'work',
+        lightColors: {
+          main: '#f91c45',
+          container: '#ffd2dc',
+          onContainer: '#59000d',
+        },
+        darkColors: {
+          main: '#ffc0cc',
+          onContainer: '#ffdee6',
+          container: '#a24258',
+        },
+      },
+      leisure: {
+        colorName: 'leisure',
+        lightColors: {
+          main: '#1cf9b0',
+          container: '#dafff0',
+          onContainer: '#004d3d',
+        },
+        darkColors: {
+          main: '#c0fff5',
+          onContainer: '#e6fff5',
+          container: '#42a297',
+        },
+      },
+      school: {
+        colorName: 'school',
+        lightColors: {
+          main: '#1c7df9',
+          container: '#d2e7ff',
+          onContainer: '#002859',
+        },
+        darkColors: {
+          main: '#c0dfff',
+          onContainer: '#dee6ff',
+          container: '#426aa2',
+        },
+      },
+    },
     events: [
       {
         id: '1',
         title: 'Event 1',
-        start: '2025-01-20',
-        end: '2025-01-20',
+        start: '2025-01-20 12:00',
+        end: '2025-01-20 15:00',
+        calendarId: 'leisure',
       },
     ],
-    selectedDate: '2025-01-20',
+    locale: 'pt-BR',
     plugins: [
       eventsService,
-      modalPlugin,
+      eventModal,
     ]
   })
 
